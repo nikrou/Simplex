f6e84969bb535cd66d7fb37b9b96e8725ae70cca
Nicolas <nikrou77@gmail.com>
2013-05-17 12:18:41 +0200

    part 6

--- src/app.php
+++ src/app.php
@@ -11,18 +11,11 @@ function is_leap_year($year = null) {
 }
 
 $routes = new Routing\RouteCollection();
-$routes->add('leap_year', 
-             new Routing\Route('/is_leap_year/{year}', 
-                               array('year' => null,
-                                     '_controller' => function ($request) {
-                                       if (is_leap_year($request->attributes->get('year'))) {
-                                         return new Response('Yep, this is a leap year!');
-                                       }
-                                       
-                                       return new Response('Nope, this is not a leap year.');
-                                     }
-                                     )
-                               )
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', 
+                                            array('year' => null,
+                                                  '_controller' => 'LeapYearController::indexAction'
+                                                  )
+                                            )
              );
  
 return $routes;
