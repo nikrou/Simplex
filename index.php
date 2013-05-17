76b1fe1817acefa241ecde5ee708d7820d92c03d
Nicolas <nikrou77@gmail.com>
2013-05-17 11:39:35 +0200

    part 2

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 
 // index.php
 
-$input = $_GET['name'];
+require_once __DIR__.'/autoload.php';
  
-printf('Hello %s', $_GET['name']);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+ 
+$request = Request::createFromGlobals();
+ 
+$input = $request->get('name', 'World');
+ 
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+ 
+$response->send();
